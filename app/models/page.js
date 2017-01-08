b2422c62c4aa324b812d5dfe53535d550ecd3ab8
javieranselmi <javieranselmi@gmail.com>
2017-01-08 13:11:44 -0300

    Unused files and methods clean-up

--- app/models/page.js
+++ app/models/page.js
@@ -1,11 +1,7 @@
 var bookshelf = require('../bookshelf');
 
 var Page = bookshelf.Model.extend({
-    tableName: 'pages',
-    create: function(values) {
-        return new Page(values)
-            .save(null, {method: "insert"});
-    }
+    tableName: 'pages'
 });
 
 module.exports = Page;
