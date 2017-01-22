a02cb2d32813f12881154d6a9b95a5fe0f2926f1
javieranselmi <javieranselmi@gmail.com>
2017-01-22 14:23:43 -0300

    fix for oauth

--- app/services/oauth.js
+++ app/services/oauth.js
@@ -1,7 +1,7 @@
 var google = require('googleapis');
 var tokenManager = require('./token-manager');
 var OAuth2Client = google.auth.OAuth2;
-var clientSecret = require('../../.client_secret.json');
+var clientSecret = require('../../client_secret.json');
 var oauth2Client = new OAuth2Client(clientSecret.web['client_id'], clientSecret.web['client_secret'], clientSecret.web['redirect_uris'][0]);
 var Promise = require('bluebird');
 
