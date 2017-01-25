b1e995dca7d98a77cad770a7237d3c3acdcc83e7
javieranselmi <javieranselmi@gmail.com>
2017-01-25 19:48:07 -0300

    Fixing .config dependency

--- server.js
+++ server.js
@@ -2,26 +2,17 @@
 
 require('dotenv').config();
 
-const fs = require('fs');
 const join = require('path').join;
 const express = require('express');
-const config = require('./config');
 const winston = require('winston');
-
 const models = join(__dirname, 'app/models');
 const port = process.env.PORT || 3000;
+
 var bodyParser = require('body-parser');
 var errorHandler = require('./app/middlewares/error-handler');
 
 const app = express();
 const Routes = require('./app/routes');
-
-// // Bootstrap models
-// fs.readdirSync(models)
-//   .filter(file => ~file.indexOf('.js'))
-//   .forEach(file => require(join(models, file)));
-
-// Bootstrap routes
 
 app.use(bodyParser.json({ type: 'application/json' }));
 app.use(Routes);
