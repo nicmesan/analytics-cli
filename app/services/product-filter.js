7b7223ddf8b6a6333dcff65ec759121084d2db30
javieranselmi <javieranselmi@gmail.com>
2017-01-27 14:55:04 -0300

    preparing database for negative keywords filter

--- app/services/product-filter.js
+++ app/services/product-filter.js
@@ -8,6 +8,13 @@ function filterProducts(clientId) {
         '(SELECT description FROM products))');
 };
 
+function filterProducts(clientId) {
+    return knex.raw('INSERT INTO target_ksets (`keys`,clientId)' +
+        ' (SELECT REPLACE(`keys`, \' \', \'-\'),' + clientId + ' ' +
+        'FROM ksets WHERE `keys` IN ' +
+        '(SELECT description FROM products))');
+};
+
 //Public
 module.exports = {
     filterProducts: filterProducts
