35ffec893cff1b4977f89537ec07337e65917766
javieranselmi <javieranselmi@gmail.com>
2017-02-21 14:20:40 -0300

    mocking knex adding first successful test

--- test/services/analytics.js
+++ test/services/analytics.js
@@ -5,19 +5,37 @@ var expect = require('chai').expect;
 var sinon = require('sinon');
 var nock = require('nock');
 var mockery = require('mockery');
+var analytics = require('../../app/services/analytics');
 
 describe('Analytics', function () {
 
-    var knex;
 
-    beforeEach(function(){
-        //Mock Knex
+    before(function(){
+        this.knex = require('../../config/knex');
+        this.mockDb = require('mock-knex');
+        this.db = this.knex({
+            client: 'sqlite'
+        });
+        this.mockDb.mock(this.db);
+        this.tracker = this.mockDb.getTracker();
+        this.tracker.install();
+        this.tracker.on('query', function checkResult(query) {
+            query.response([
+                {
+                    viewId : '123'
+                }
+            ]);
+        });
     });
 
     describe('#getViewIdByClientId', function() {
+
        it('should get from the database the viewId with the given clientId', function() {
-            expect(1).to.equal(1);
-       })
+           analytics.getViewIdByClientId(1).then((viewId) => {
+               assert.equal(viewId, '123');
+           })
+       });
+
     });
 
 });
