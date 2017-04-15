4f31cbca037ca33b463d59c9670a5a0ac56ccbb2
Nicolas Medina <nicolas.medinasanchez@fdvsolutions.com>
2017-04-15 00:16:53 -0300

    controller cleaned up, functions now in lib folder

--- app/controllers/save-pages-by-value.js
+++ app/controllers/save-pages-by-value.js
@@ -1,13 +1,11 @@
 var Pages = require('../collections/pages');
-var analytics = require('../integrations/analytics');
-var auth = require('../integrations/oauth');
-var Promise = require('bluebird');
 var errors = require('../errors');
+var analytics = require('../lib/analytics');
 
 exports.saveTopValuePages = function (req, res, next) {
     var clientId = req.params.clientId;
 
-    return getPages(req.body.pageSize, req.params.clientId, req.body.orderBy)
+    return analytics.getPages(req.body.pageSize, req.params.clientId, req.body.orderBy)
         .then(function (data) {
             var dataToSave = data.reports[0].data.rows;
             if (!dataToSave) {
@@ -36,23 +34,6 @@ exports.saveTopValuePages = function (req, res, next) {
         })
 };
 
-function getPages(pageSize, clientId, orderBy) {
-
-    if (!pageSize || !clientId) {
-        throw errors.httpError('Include page size and client ID');
-    }
-
-    var options = getAnalyticsOptions(pageSize, orderBy);
-
-    return analytics.getViewIdByClientId(clientId)
-        .then(function (viewId) {
-            return analytics.fetch(viewId, options)
-        })
-        .catch(function (error) {
-            throw errors.httpError("Get pages error", error);
-        });
-}
-
 function formatPageRow(row, clientId) {
     return {
         pageValue: row.metrics[0].values[0],
@@ -60,29 +41,4 @@ function formatPageRow(row, clientId) {
         sessions: row.metrics[0].values[1],
         clientId: clientId
     }
-}
-
-function getAnalyticsOptions(pageSize, orderBy) {
-    return {
-        pageSize: pageSize,
-        metrics: [
-            {"expression": "ga:pageValue"},
-            {"expression": "ga:sessions"}
-        ],
-        dimensions: [
-            {name: 'ga:pagePath'},
-            {name: "ga:segment"}
-        ],
-        orderBys: [
-            {
-                "sortOrder": "DESCENDING",
-                "fieldName": orderBy || "ga:pageValue"
-            }
-        ],
-        segments: [
-            {
-                "segmentId": "gaid::TRNU4qP8Q6K5L8nPDicJaA"
-            }
-        ]
-    };
 }
