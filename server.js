30ebba2ae27e2ab307025e5b2a817fb0470406cd
javieranselmi <javieranselmi@gmail.com>
2017-04-29 20:33:32 -0300

    Adding request log

--- server.js
+++ server.js
@@ -5,9 +5,11 @@ require('dotenv').config();
 const join = require('path').join;
 const express = require('express');
 const winston = require('winston');
+const requestLogger = require('./app/middlewares/request-logger');
 var bodyParser = require('body-parser');
 var errorHandler = require('./app/middlewares/error-handler');
 var setCredentials = require('./app/middlewares/set-credentials-middleware');
+
 
 const models = join(__dirname, 'app/models');
 const port = process.env.PORT || 3000;
@@ -16,6 +18,7 @@ const app = express();
 const Routes = require('./app/routes');
 
 app.use(bodyParser.json({ type: 'application/json' }));
+app.use(requestLogger);
 app.use('/google-api/*/:clientId', setCredentials);
 app.use(Routes);
 app.use(errorHandler);
