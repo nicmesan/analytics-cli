e6be2e7757255fffd896d0838686a38c5876bb6b
javieranselmi <javieranselmi@gmail.com>
2017-04-30 11:26:37 -0300

    enable CORS

--- server.js
+++ server.js
@@ -6,6 +6,7 @@ const join = require('path').join;
 const express = require('express');
 const winston = require('winston');
 const requestLogger = require('./app/middlewares/request-logger');
+const cors = require('./app/middlewares/cors');
 var bodyParser = require('body-parser');
 var errorHandler = require('./app/middlewares/error-handler');
 var setCredentials = require('./app/middlewares/set-credentials-middleware');
@@ -19,6 +20,7 @@ const Routes = require('./app/routes');
 
 app.use(bodyParser.json({ type: 'application/json' }));
 app.use(requestLogger);
+app.use(cors);
 app.use('/google-api/*/:clientId', setCredentials);
 app.use(Routes);
 app.use(errorHandler);
