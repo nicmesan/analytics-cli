62c280c90e619ae5c7f9938f5d080808c16b7953
javieranselmi <javieranselmi@gmail.com>
2017-05-28 17:35:44 -0300

    Adding final classes filtering

--- app/services/keyword-similarity-filter.js
+++ app/services/keyword-similarity-filter.js
@@ -30,25 +30,63 @@ function truncateProductMatchesSimilarity() {
     return knex("product_matches_similarity").truncate();
 }
 
-function processEqClass() {
-    return getAndDeleteFirstEquivalenceClass()
-        .tap((eqClass) => {
-            console.log('got equivalence class', eqClass);
-        }).then((eqClass) => {
-            return similarityFilter(eqClass);
-        }).then(() => {
-            return similarityFinalClassesInsert();
-        }).then(() => {
-            return deleteFromGroupedUnique();
-        }).then(() => {
-            return truncateProductMatchesSimilarity();
+function countProductMatchesGroupedUnique() {
+    return knex("product_matches_grouped_unique").count("originalKeywordId")
+        .then((eqClass) => {
+            return eqClass[0]['count(`originalKeywordId`)'];
         });
 };
+
+
+function processEqClass() {
+    return countProductMatchesGroupedUnique().then((count) => {
+        console.log('remaining unique classes count: ', count);
+        if (count == 0) {
+            return true;
+        } else {
+            return getAndDeleteFirstEquivalenceClass()
+                .tap((eqClass) => {
+                    console.log('Got equivalence class for keyword ', eqClass.originalKeywordId, ' with value ', eqClass.keywordValue);
+                }).then((eqClass) => {
+                    return similarityFilter(eqClass);
+                }).then(() => {
+                    return similarityFinalClassesInsert();
+                }).then(() => {
+                    return deleteFromGroupedUnique();
+                }).then(() => {
+                    return truncateProductMatchesSimilarity();
+                }).then(() => {
+                    return processEqClass();
+                });
+        }
+    });
+};
+
+
+function matchesToMatchesGrouped() {
+    winston.info("Generating grouped product matches.");
+    return knex.raw(queries.matchesToMatchesGrouped);
+};
+
+
+function matchesGroupedToMatchesGroupedUnique() {
+    winston.info("Generating unique grouped product matches.");
+    return knex.raw(queries.matchesGroupedToMatchesGroupedUnique);
+};
+
+
 module.exports = function (clientId) {
 
     winston.info("Initiating keyword similarity filter");
 
-    return processEqClass();
+    return matchesToMatchesGrouped()
+        .then(() => {
+            return matchesGroupedToMatchesGroupedUnique();
+        })
+        .then(() => {
+            return processEqClass();
+        });
+
 
     //return Promise.resolve(1); //Placeholder
 
